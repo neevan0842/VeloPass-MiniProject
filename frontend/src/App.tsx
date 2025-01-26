3676c981d8297af4cd92f6a57eb0124d4fddadda
Naveen Manoj <manojnaveen085@gmail.com>
2025-01-26 21:48:45 +0530

    Basic frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
